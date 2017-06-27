ee82cf2685aca955d9e158b143bec241556b5708
madhumithaj <madhuj5296@gmail.com>
2017-06-27 11:29:07 +0000

    java file
    java files fr trial

---
+++ MainClass.java
@@ -0,0 +1,12 @@
+import java.util.Calendar;
+import java.util.Formatter;
+
+public class MainClass{
+   public static void main(String args[]) {
+      Formatter fmt = new Formatter();
+      Calendar cal = Calendar.getInstance();
+      fmt = new Formatter();
+      fmt.format("%tl:%tM", cal, cal);
+      System.out.println(fmt);
+   }
+}
